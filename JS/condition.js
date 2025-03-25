e0de05935ebfddd51beb07731ef4b714b4e90155
mdshakil04 <m.shakil04@yahoo.com>
2025-03-26 01:30:49 +0600

    Another condition done

--- JS/condition.js
+++ JS/condition.js
@@ -1,11 +1,17 @@
-const price = 1250;
+const price = 4000;
 const member = true;
 
-if( price >= 10000 || member == true){
-    console.log("You will get 5% discount")
+if( price >= 10000 && member == true){
+    console.log("You will get 10% discount")
+    const discount = price * 0.1;
+    const finalPrice = price - discount;
+    console.log("Your final price after discount is: ", finalPrice)
+}else if(price > 5000){
+    console.log("You get 5% discount")
     const discount = price * 0.05;
     const finalPrice = price - discount;
     console.log("Your final price after discount is: ", finalPrice)
 }else{
-    console.log("You have to pay full BDT")
+    console.log("Sorry! No discount available")
+    console.log("Your final price is: ", price)
 }
