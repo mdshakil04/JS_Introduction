00fe3b2aa73362e7c2f404bc4d6ba6e1ad6d5de1
mdshakil04 <m.shakil04@yahoo.com>
2025-03-25 23:10:11 +0600

    if else practice

--- JS/intro.js
+++ JS/intro.js
@@ -1,5 +1,10 @@
 // console.log("Bismillah")
-let price = 32;
+let price = 132;
 price = price + 10; // price += 10;
+if(price > 50){
+    console.log("I will buy Banana")
+}else{
+    console.log("I will buy Potato")
+}
 
 // console.log(price)
