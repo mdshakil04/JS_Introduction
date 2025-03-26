ff27d57904a77763334c9c0891c7d3ca51a1986c
mdshakil04 <m.shakil04@yahoo.com>
2025-03-26 23:43:35 +0600

    Ternary if-else done

--- JS/condition.js
+++ JS/condition.js
@@ -1,18 +1,26 @@
-const price = 4000;
+const price = 1000;
 const member = true;
 
-if( price >= 10000 && member == true){
-    console.log("You will get 10% discount")
-    const discount = price * 0.1;
-    const finalPrice = price - discount;
-    console.log("Your final price after discount is: ", finalPrice)
-}else if(price > 5000){
-    console.log("You get 5% discount")
-    const discount = price * 0.05;
-    const finalPrice = price - discount;
-    console.log("Your final price after discount is: ", finalPrice)
-}else{
-    console.log("Sorry! No discount available")
-    console.log("Your final price is: ", price)
-}
-// BMI Calculator will calculate
+// if( price >= 1000 && member == true){
+//     // console.log("You will get 10% discount")
+//     const discount = price * 0.1;
+//     const finalPrice = price - discount;
+//     // console.log("Your final price after discount is: ", finalPrice)
+// }else if(price > 5000){
+//     // console.log("You get 5% discount")
+//     const discount = price * 0.05;
+//     const finalPrice = price - discount;
+//     // console.log("Your final price after discount is: ", finalPrice)
+// }else{
+//     // console.log("Sorry! No discount available")
+//     // console.log("Your final price is: ", price)
+// }
+/*
+Ternary if-else
+
+condition ? do something when true : do something when false
+
+const finalPrice = price >= 10000 && member == true? price - (price * 0.1) : price > 5000? price - (price * 0.05) : price;
+
+*/
+price > 1000 ? console.log("I can't effort this") : console.log("I will buy this")
