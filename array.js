14be6410dae99da65a0c2a139b1589981c62893a
mdshakil04 <m.shakil04@yahoo.com>
2025-04-04 12:23:41 +0600

    new code added

--- array.js
+++ array.js
@@ -1,5 +1,10 @@
 const mixedArray = ["Abdul", 12, true, 23.56]
-console.log(mixedArray.length)
+// console.log(mixedArray.length)
 // console.log(mixedArray.indexOf("Abdul"))
-const abdulPosition = mixedArray.indexOf('Abdul')
-console.log(abdulPosition)
+// const abdulPosition = mixedArray.indexOf('Abdul')
+// console.log(mixedArray[3])
+// console.log(abdulPosition)
+//  mixedArray.push = 23;
+// const newUpadhan = mixedArray.push(24);
+console.log(mixedArray)
+// console.log(newUpadhan)
