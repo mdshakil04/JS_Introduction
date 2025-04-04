e5a85b343940ad442764d2e5d500066ebb0da462
mdshakil04 <m.shakil04@yahoo.com>
2025-04-04 10:43:28 +0600

    Ternary-condition done

--- if-else.js
+++ if-else.js
@@ -1,9 +1,12 @@
-let fees =1000;
-const isStudent = false;
+let fees = 1000;
+const isStudent = true;
 if( isStudent === true){
     fees = fees + 100;
 }
 else{
     fees = 0;
 }
-console.log("Tumi diba " + fees)
+// console.log("Tumi diba " + fees)
+
+fees = isStudent === true ? fees+100 : fees = 0;
+console.log("Tumi diba "+fees)
