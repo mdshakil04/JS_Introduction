4c1d8cff8b0d7a4a33df42f7a024c09c37fc5b42
mdshakil04 <m.shakil04@yahoo.com>
2025-04-04 12:18:09 +0600

    new file added

---
+++ array.js
@@ -0,0 +1,5 @@
+const mixedArray = ["Abdul", 12, true, 23.56]
+console.log(mixedArray.length)
+// console.log(mixedArray.indexOf("Abdul"))
+const abdulPosition = mixedArray.indexOf('Abdul')
+console.log(abdulPosition)
