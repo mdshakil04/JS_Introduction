712878aa5d7140831f268dd402a01e637d7f0162
mdshakil04 <m.shakil04@yahoo.com>
2025-04-05 21:17:18 +0600

    Some array method done

--- array.js
+++ array.js
@@ -12,12 +12,23 @@ const mixedArray = ["Abdul", 12, true, "Kabul"]
 // console.log(newUpadhan)
 // Code updated to 18_5_3
 // console.log(mixedArray.length)
-// mixedArray.push("Shakil");
+// 
 // console.log(mixedArray)
-// We can use shift() to remove first element from an array
-const removeFirstElement = mixedArray.shift()
-console.log("This First removed element is " + removeFirstElement)
-// We can use pop() to remove last element from an array
+ /*
+ > To add an element last on an array we use push() Method
+ > To add an element first on an array we use unshift() method
+ > We can use shift() to remove first element from an array
+ > We can use pop() to remove last element from an array
+ */
+/*mixedArray.push("Shakil");
+console.log(mixedArray)
+
+mixedArray.unshift("Akhi")
+console.log(mixedArray)
+
+ const removeFirstElement = mixedArray.shift()
+ console.log("This First removed element is " + removeFirstElement)
+
 const removeLastElement = mixedArray.pop()
 console.log("This last removed element is " + removeLastElement)
-// console.log(mixedArray)
+*/
