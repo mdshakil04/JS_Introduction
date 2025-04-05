e080690160f3ff93dbf479815e21ca2ccb459219
mdshakil04 <m.shakil04@yahoo.com>
2025-04-05 20:54:48 +0600

    push method

--- array.js
+++ array.js
@@ -7,8 +7,10 @@ console.log(mixedArray)
 // console.log(abdulPosition)
 //  mixedArray.push = 23;
 // const newUpadhan = mixedArray.push(24);
-mixedArray[2] = "Shakil"
-console.log(mixedArray)
+// mixedArray[2] = "Shakil"
+// console.log(mixedArray)
 // console.log(newUpadhan)
 // Code updated to 18_5_3
-console.log(mixedArray.length)
+// console.log(mixedArray.length)
+mixedArray.push("Shakil");
+console.log(mixedArray)
