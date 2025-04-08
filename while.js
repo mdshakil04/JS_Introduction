49d2f250c35c5ae4408bbd379adf00e668fed48c
mdshakil04 <m.shakil04@yahoo.com>
2025-04-09 00:03:21 +0600

    Loop related problem

--- while.js
+++ while.js
@@ -1,6 +1,12 @@
-let number = 0;
+/*let number = 0;
 while(number < 5){
     console.log("Now the Number is", number)
     number++
 }
-console.log('Now the number is', number, 'and its out of loop')
+console.log('Now the number is', number, 'and its out of loop')*/
+let num = 1;
+while (num < 10){
+    console.log("I want to see odd number")
+    num +=2
+    console.log(num)
+}
