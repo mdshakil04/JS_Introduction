c2f5925c6b03d66b4876a8203ccb63c101364b6f
mdshakil04 <m.shakil04@yahoo.com>
2025-04-09 16:18:52 +0600

    Subtask 4 done

--- loop-task.js
+++ loop-task.js
@@ -11,5 +11,13 @@ for(let i = 78 ; i <= 98; i+=2){
 let sum = 0;
 for(let i = 91; i <= 129; i+=2){
     sum += i
+    // console.log(i)
 }
-console.log(sum)
+// console.log(sum)
+// Subtask 4
+let evenSum = 0;
+for(let i = 52 ; i <= 85; i +=2 ){
+    evenSum += i
+    // console.log(i)
+}
+console.log(evenSum)
