bc27a698426daa24a95bc365d2d2714146ee67df
mdshakil04 <m.shakil04@yahoo.com>
2025-04-09 15:35:12 +0600

    Some sum related problem solved

--- loop.js
+++ loop.js
@@ -5,9 +5,15 @@
 // }
 
 // console.log("The sum of numbers from 1 to 100 is:", sum);
-let sum = 0;
+/*let sum = 0;
 for(let i = 1; i <=100; i++){
     sum += i
     // console.log(i)
+}
+console.log(sum)*/
+let sum = 0;
+for(let i = 1; i <=50; i++){
+    
+    sum = sum + i
 }
 console.log(sum)
