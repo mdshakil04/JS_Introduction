9a04add5498449426658b7f9eca171fca1d89a03
mdshakil04 <m.shakil04@yahoo.com>
2025-04-09 16:09:55 +0600

    Subtask 3 done

--- loop-task.js
+++ loop-task.js
@@ -5,5 +5,11 @@ for( let i = 61 ; i <= 100; i+=2){
 }
 // Subtask 2
 for(let i = 78 ; i <= 98; i+=2){
-    console.log(i)
+    // console.log(i)
 }
+// Subtask 3
+let sum = 0;
+for(let i = 91; i <= 129; i+=2){
+    sum += i
+}
+console.log(sum)
