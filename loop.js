b600987f2098cc9f742bf5f05e6813285fa2b77e
mdshakil04 <m.shakil04@yahoo.com>
2025-04-09 14:39:13 +0600

    Do sum using loop

---
+++ loop.js
@@ -0,0 +1,13 @@
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     sum += i;
+// }
+
+// console.log("The sum of numbers from 1 to 100 is:", sum);
+let sum = 0;
+for(let i = 1; i <=10; i++){
+    sum += i
+    console.log(i)
+}
+console.log(sum)
