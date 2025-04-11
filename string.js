195a286d0c995ccb575c4e26e4ff89104037ca07
mdshakil04 <m.shakil04@yahoo.com>
2025-04-11 10:36:32 +0600

    Includes

--- string.js
+++ string.js
@@ -1,2 +1,4 @@
 const country = 'Bangladesh';
-console.log(country.slice(0, 6 ))
+// console.log(country.slice(0, 6 ))
+// const desh = country.includes('desh')
+console.log(country.includes('desh'))
