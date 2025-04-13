c5a8ee7f3874c54b7c7eac52a2ff1a3c8deb83de
mdshakil04 <m.shakil04@yahoo.com>
2025-04-13 08:38:05 +0600

    all aboutreverse

--- revers.js
+++ revers.js
@@ -1,8 +1,21 @@
 const numbers =[1,2,3,4,5,6,7,8,9]
 const rev_numbers = [];
+ /*
 for(const num of numbers){
     // console.log(num)
     // rev_numbers.push(num)
     rev_numbers.unshift(num)
 }
 console.log(rev_numbers)
+*/
+for(let i =0; i< numbers.length; i++){
+    // console.log(numbers[i])
+    const num = numbers[i]
+    rev_numbers.unshift(num)
+}
+console.log(rev_numbers)
+
+for(let i = numbers.length-1; i >= 0 ; i--){
+    const num = numbers[i]
+    console.log(num)
+}
