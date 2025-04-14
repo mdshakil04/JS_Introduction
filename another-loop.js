375da267c60b1446b64bbc9329ad5e71a9cf17a7
mdshakil04 <m.shakil04@yahoo.com>
2025-04-15 00:08:10 +0600

    Task text added

--- another-loop.js
+++ another-loop.js
@@ -6,6 +6,7 @@ for (let i = colors.length - 1; i >= 0; i--) {
 }
 
 // console.log(reversedColors);
+// Write a JavaScript code to get the even numbers from an array using any looping technique.
 const numbers = [12, 98, 5, 41, 23, 78, 46];
 const evenNumber = [];
 for(let i = 0 ; i< numbers.length; i++){
@@ -14,6 +15,7 @@ for(let i = 0 ; i< numbers.length; i++){
     }
 }
 // console.log(evenNumber)
+// Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
 const statement = 'I am a hard working person';
 const reversedStatement = statement.split(' ').reverse().join(' ');
 
