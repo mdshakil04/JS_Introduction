b033b010e643564549fcfa32fc8cb2cc4001a7ef
mdshakil04 <m.shakil04@yahoo.com>
2025-04-14 23:18:18 +0600

    Another loop related task

---
+++ another-loop.js
@@ -0,0 +1,8 @@
+const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+const reversedColors = [];
+
+for (let i = colors.length - 1; i >= 0; i--) {
+  reversedColors.push(colors[i]);
+}
+
+console.log(reversedColors);
