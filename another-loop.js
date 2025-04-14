b2981744720459698bd13d0009f5c3ac684c5053
mdshakil04 <m.shakil04@yahoo.com>
2025-04-14 23:56:22 +0600

    Reverse related task

--- another-loop.js
+++ another-loop.js
@@ -5,4 +5,12 @@ for (let i = colors.length - 1; i >= 0; i--) {
   reversedColors.push(colors[i]);
 }
 
-console.log(reversedColors);
+// console.log(reversedColors);
+const numbers = [12, 98, 5, 41, 23, 78, 46];
+const evenNumber = [];
+for(let i = 0 ; i< numbers.length; i++){
+    if( numbers[i] % 2 === 0 ){
+        evenNumber.push(numbers[i])
+    }
+}
+console.log(evenNumber)
